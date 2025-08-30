a2ae1629380762b37066d7f9ae599c6b954a2cbf
PRINCE200016 <arjunrajawat28@gmail.com>
2025-08-31 00:36:45 +0530

    after fixing erros of deployements

--- next.config.ts
+++ next.config.ts
@@ -28,7 +28,7 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
-       {
+      {
         protocol: 'https',
         hostname: 'drive.google.com',
         port: '',
